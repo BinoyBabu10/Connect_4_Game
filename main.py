b979aae9500a294920092205fbeb82523080b696
Your Name <you@example.com>
2023-12-10 14:57:29 +0530

    Taking input from players

---
+++ main.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def create_board():
+    board=np.zeros((6,7))
+    return board
+
+board=create_board()
+game_over=False
+turn=0
+
+while not game_over:
+#     ask for player1 input
+    if turn==0:
+        selection=int(input("Player1 make your selection(0-6)"))
+
+    else:
+        selection = int(input("Player1 make your selection(0-6)"))
