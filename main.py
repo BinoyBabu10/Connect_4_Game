54a1253d6c0773475ec16a0cee02686ad3c305b8
Your Name <you@example.com>
2023-12-10 16:45:55 +0530

    Check Winning Completed

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def drop_piece(board,row,col,piece):
     board[row][col]=piece
 
 def is_valid_location(board,col):
-    return board[5][col]==0
+    return board[ROW_COUNT-1][col]==0
 
 def get_next_open_row(board,col):
     for r in range(ROW_COUNT):
@@ -21,6 +21,30 @@ def get_next_open_row(board,col):
 def print_board(board):
     print(np.flip(board,0))
 
+def winning_move(board,piece):
+    #check all of horizontal location
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT):
+            if board[r][c]==piece and board[r][c+1]==piece and board[r][c+2]==piece and board[r][c+3]==piece:
+                return True
+
+#   check all vertical location
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT-3):
+            if board[r][c]==piece and board[r+1][c]==piece and board[r+2][c]==piece and board[r+3][c]==piece:
+                return True
+
+#   check positively sloped diagnal
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT-3):
+            if board[r][c]==piece and board[r+1][c+1]==piece and board[r+2][c+2]==piece and board[r+3][c+3]==piece:
+                return True
+
+#   check negatively sloped diagnal
+    for c in range(COLUMN_COUNT-3):
+        for r in range(3,ROW_COUNT):
+            if board[r][c]==piece and board[r-1][c+1]==piece and board[r-2][c+2]==piece and board[r-3][c+3]==piece:
+                return True
 board=create_board()
 print_board(board)
 game_over=False
@@ -33,6 +57,11 @@ while not game_over:
         if is_valid_location(board,col):
             row=get_next_open_row(board,col)
             drop_piece(board,row,col,1)
+
+            if winning_move(board,1):
+                print("Player 1 wins")
+                game_over=True
+                break
     #  ask for player 2 input
     else:
         col= int(input("Player2 make your selection(0-6)"))
