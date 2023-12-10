cb67153f5a26fbec3b4caa1979015618c4b88d74
Your Name <you@example.com>
2023-12-10 15:29:32 +0530

    Structure of the game is complete

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
 import numpy as np
 
+ROW_COUNT=6
+COLUMN_COUNT=7
 def create_board():
     board=np.zeros((6,7))
     return board
 
+def drop_piece(board,row,col,piece):
+
+    board[row][col]=piece
+
+def is_valid_location(board,col):
+    return board[5][col]==0
+
+def get_next_open_row(board,col):
+    for r in range(ROW_COUNT):
+        if board[r][col]==0:
+            return r
+
+def print_board(board):
+    print(np.flip(board,0))
+
 board=create_board()
+print_board(board)
 game_over=False
 turn=0
 
 while not game_over:
 #     ask for player1 input
     if turn==0:
-        selection=int(input("Player1 make your selection(0-6)"))
-
+        col=int(input("Player1 make your selection(0-6):"))
+        if is_valid_location(board,col):
+            row=get_next_open_row(board,col)
+            drop_piece(board,row,col,1)
+    #  ask for player 2 input
     else:
-        selection = int(input("Player1 make your selection(0-6)"))
+        col= int(input("Player2 make your selection(0-6)"))
+        if is_valid_location(board,col):
+            row=get_next_open_row(board,col)
+            drop_piece(board,row,col,2)
+    print_board(board)
+
+    turn+=1
+    turn=turn%2
